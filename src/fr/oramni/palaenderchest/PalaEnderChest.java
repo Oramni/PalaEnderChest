e32d918327b99a80f8a0cd704743a8435bd3a8d8
Oramni <Oramni@192.168.1.14>
2016-06-25 17:33:40 +0200

    Test

---
+++ src/fr/oramni/palaenderchest/PalaEnderChest.java
@@ -0,0 +1,13 @@
+package fr.oramni.palaenderchest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PalaEnderChest extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+		
+		
+	}
+
+}

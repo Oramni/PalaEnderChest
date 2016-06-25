6726eebdb9265949abe29433bf042c9e565d258b
Oramni <Oramni@192.168.1.14>
2016-06-25 17:36:37 +0200

    Test2

--- src/fr/oramni/palaenderchest/PalaEnderChest.java
+++ src/fr/oramni/palaenderchest/PalaEnderChest.java
@@ -1,5 +1,8 @@
 package fr.oramni.palaenderchest;
 
+
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PalaEnderChest extends JavaPlugin {
@@ -7,6 +10,8 @@ public class PalaEnderChest extends JavaPlugin {
 	@Override
 	public void onEnable(){
 		
+		Logger logger = getLogger();
+		logger.info("§aOK !");
 		
 	}
 

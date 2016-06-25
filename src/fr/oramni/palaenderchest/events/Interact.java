cb83b9197ddb47b30d6e2bdea6be28919f52521e
Oramni <Oramni@192.168.1.14>
2016-06-25 18:34:49 +0200

    - Ajout de l'animation de l'ouverture de l'enderchest

--- src/fr/oramni/palaenderchest/events/Interact.java
+++ src/fr/oramni/palaenderchest/events/Interact.java
@@ -1,8 +1,13 @@
 package fr.oramni.palaenderchest.events;
 
+import net.minecraft.server.v1_7_R4.Blocks;
+import net.minecraft.server.v1_7_R4.PacketPlayOutBlockAction;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,6 +30,10 @@ public class Interact implements Listener {
 		Inventory inv = Bukkit.createInventory(null, 9, "§cPalaEnderChest (§6"+p.getName()+"§c)");
 		for(Player ps : Bukkit.getOnlinePlayers()){
 			ps.playSound(e.getClickedBlock().getLocation(), Sound.CHEST_OPEN, 1, 1);
+		Block block = e.getClickedBlock();
+		CraftPlayer cp = (CraftPlayer) ps;
+		PacketPlayOutBlockAction packet = new PacketPlayOutBlockAction((int)block.getLocation().getX(),(int)block.getLocation().getY(),(int)block.getLocation().getZ(), Blocks.ENDER_CHEST, 1 ,2);
+		cp.getHandle().playerConnection.sendPacket(packet); 
 		}
 		p.updateInventory();
 		p.openInventory(inv);

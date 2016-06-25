037d35214cd812af44cacfb65b985a02fbd4e4b2
Oramni <Oramni@192.168.1.14>
2016-06-25 21:55:11 +0200

    - Ajout de la commande /Enderchest (/ec) avec permission

--- src/fr/oramni/palaenderchest/PalaEnderChest.java
+++ src/fr/oramni/palaenderchest/PalaEnderChest.java
@@ -1,15 +1,34 @@
 package fr.oramni.palaenderchest;
 
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PalaEnderChest extends JavaPlugin {
 	
 	@Override
 	public void onEnable(){
+		
 		EventManager.registerEvents(this);
 	}
 	
-	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		
+		if(label.equalsIgnoreCase("enderchest")){
+			Player p = (Player) sender;
+			if(p.hasPermission("palaenderchest.cmd")){
+			PalaInv.openEnder(p);
+			return true;
+			} else {
+				p.sendMessage("§cVous n'avez pas les droits de faire cette commande.");
+				return true;
+			}
+		}
+		
+		
+		return false;
+	}
 
 }
